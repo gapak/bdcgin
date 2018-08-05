bd1337c73f4f6d8d82da25e4c1462f651e619923
Alex Gapak <iamgapak@gmail.com>
2018-08-05 20:31:09 +0300

    Rebalance

--- src/game/weapons.js
+++ src/game/weapons.js
@@ -3,12 +3,12 @@ import _ from 'lodash';
 
 
 const weapons_bodies = {
-    sword:  {name: "Sword",    min_dmg: 4, max_dmg: 6, accuracy: 5, range: 4, speed: 17},
-    spear:  {name: "Spear",    min_dmg: 2, max_dmg: 3, accuracy: 6, range: 6, speed: 14},
-    axe:    {name: "Axe",      min_dmg: 5, max_dmg: 11,accuracy: 2, range: 2, speed: 18},
-    hammer: {name: "Hammer",   min_dmg: 6, max_dmg: 9, accuracy: 1, range: 1, speed: 16},
-    bow:    {name: "Bow",      min_dmg: 1, max_dmg: 6, accuracy: 4, range: 18, speed: 20},
-    cross:  {name: "Arbalest", min_dmg: 3, max_dmg: 7, accuracy: 3, range: 12, speed: 22},
+    sword:  {name: "Sword",    min_dmg: 2, max_dmg: 5, accuracy: 5, range: 3, speed: 17},
+    spear:  {name: "Spear",    min_dmg: 1, max_dmg: 3, accuracy: 6, range: 6, speed: 14},
+    axe:    {name: "Axe",      min_dmg: 3, max_dmg: 7, accuracy: 2, range: 2, speed: 18},
+    hammer: {name: "Hammer",   min_dmg: 4, max_dmg: 6, accuracy: 1, range: 1, speed: 16},
+    bow:    {name: "Bow",      min_dmg: 1, max_dmg: 3, accuracy: 4, range: 24, speed: 20},
+    cross:  {name: "Arbalest", min_dmg: 2, max_dmg: 4, accuracy: 3, range: 16, speed: 22},
 };
 
 const weapons_quality = {
@@ -21,6 +21,7 @@ const weapons_quality = {
 };
 
 const weapons_mods = {
+    flat: {name: "Typical",       min_dmg: 0, max_dmg: 0, accuracy: 0, range: 0, speed: 0},
     min_dmg: {name: "Tuned",      min_dmg: 3, max_dmg: 0, accuracy: 0, range: 0, speed: 0},
     max_dmg: {name: "Sharped",    min_dmg: 0, max_dmg: 3, accuracy: 0, range: 0, speed: 0},
     all_dmg: {name: "Powerful",   min_dmg: 1, max_dmg: 2, accuracy: 0, range: 0, speed: 0},
@@ -34,9 +35,11 @@ const weapons_mods = {
 export const genWeapon = (level = 1) => {
     let body = _.sample(weapons_bodies);
     let q = Math.floor(Math.min(_.random(1, 6), _.random(1, Math.sqrt(level))));
-    console.log('quality', q);
     let quality = weapons_quality[q];
-    let mod = _.sample(weapons_mods);
+    let mod = (level === 1) ? weapons_mods.flat : _.sample(weapons_mods);
+
+
+    console.log('Gen Weapon: ', body, quality, mod);
 
     let new_weapon = {
         name: mod.name + ' ' + quality.name + ' ' + body.name,
